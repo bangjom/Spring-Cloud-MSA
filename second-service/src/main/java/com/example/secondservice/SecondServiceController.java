ddffa9bdb38deff029348188e644f5d326e80917
bangjom <kbj7227@gmail.com>
2021-09-23 18:14:06 +0900

    스프링 클라우드 게이트웨이 + 스프링 클라우드 유레카 + 랜덤 포트 적용

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,22 @@
 package com.example.secondservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
+    private final Environment env;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service";
@@ -22,7 +29,8 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi. there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
